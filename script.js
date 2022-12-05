f562b4f7283822bcca42f2697b90bff4f242059b
Herbert Carvalho <48456248+Herbert22@users.noreply.github.com>
2022-12-05 10:42:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.onkeydown = teclado;
+function teclado(e){
+    if (e.keyCode == 38) {				
+        document.getElementById('info').innerHTML = 'Seta para Cima!';
+    }
+    else if (e.keyCode == 40) {				
+        document.getElementById('info').innerHTML = 'Seta para Baixo!';
+    }
+    else if (e.keyCode == 37) {				
+        document.getElementById('info').innerHTML = 'Seta para Esquerda!';
+    }
+    else if (e.keyCode == 39) {				
+        document.getElementById('info').innerHTML = 'Seta para Direira!';
+    }
+}
